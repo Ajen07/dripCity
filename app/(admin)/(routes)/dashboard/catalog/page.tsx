3f907bee8c3585fcad91d537b811545ec54d0986
Arman Kumar Jena <arman.k.jena07@gmail.com>
2024-07-12 09:14:41 +0530

    added sharable url param in catalog

--- app/(admin)/(routes)/dashboard/catalog/page.tsx
+++ app/(admin)/(routes)/dashboard/catalog/page.tsx
@@ -1,9 +1,17 @@
 import CatalogTabs from "@/components/admin/catalog/catalog-tabs";
 import SearchBar from "@/components/SearchBar";
-import React from "react";
+import React, { Suspense } from "react";
 import AddProductButton from "@/components/admin/catalog/add-product-button";
+import CatalogProducts from "@/components/admin/catalog/catalog-products";
 
-const Catalogue = () => {
+const Catalogue = ({
+  searchParams,
+}: {
+  searchParams?: {
+    activeTab?: string;
+  };
+}) => {
+  const activeTab = searchParams?.activeTab || "all";
   return (
     <main className="pt-8 pl-5">
       <h1 className="text-4xl font-extrabold text-purple-500 capitalize">
@@ -17,6 +25,11 @@ const Catalogue = () => {
       </section>
       <section className="mt-4 py-4">
         <CatalogTabs />
+      </section>
+      <section>
+        <Suspense key={activeTab} fallback={<div>Loading...</div>}>
+          <CatalogProducts activeTab={activeTab} />
+        </Suspense>
       </section>
     </main>
   );
--- components/admin/catalog/catalog-tabs.tsx
+++ components/admin/catalog/catalog-tabs.tsx
@@ -1,26 +1,35 @@
 "use client";
-import React, { useState } from "react";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import React from "react";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { catalogTabs } from "@/lib/constants";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 
 const CatalogTabs = () => {
-  const [activeTab, setActiveTab] = useState<string>("all");
-
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
   const handleChange = (value: string) => {
-    setActiveTab(value);
+    const params = new URLSearchParams(searchParams);
+    if (value && value !== "all") {
+      params.set("activeTab", value);
+    }else{
+      params.delete("activeTab");
+    }
+    replace(`${pathname}?${params.toString()}`);
   };
   return (
-    <Tabs defaultValue="all" className="w-fit" onValueChange={handleChange}>
+    <Tabs defaultValue={searchParams.get('activeTab')?.toString()} className="w-fit" onValueChange={handleChange}>
       <TabsList className=" h-[50px] w-[330px]">
         {catalogTabs.map((tab) => (
-          <TabsTrigger key={tab.value} value={tab.value} className="text-[1.1rem]">
+          <TabsTrigger
+            key={tab.value}
+            value={tab.value}
+            className="text-[1.1rem]"
+          >
             {tab.name}
           </TabsTrigger>
         ))}
       </TabsList>
-      <TabsContent value={activeTab}>
-        My catalog Items
-      </TabsContent>
     </Tabs>
   );
 };
